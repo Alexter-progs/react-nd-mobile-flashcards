fb3be1038036a100031987ecf2eed49a9345d775
alexp <alexplotbox@teamlead.ru>
2017-12-16 18:21:28 +0300

    simple navigation and structure

--- App.js
+++ App.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { Constants } from 'expo'
+
+import DeckList from "./components/DeckList";
+import NewDeck from "./components/NewDeck";
+import Deck from "./components/Deck";
+
+const Tabs = TabNavigator({
+    DeckList: {
+        screen: DeckList,
+        navigationOptions: {
+            tabBarLabel: 'Decks',
+            tabBarIcon: ({tintColor}) => <MaterialCommunityIcons name='cards' size={30} color={tintColor}/>
+        }
+    },
+    NewDeck: {
+        screen: NewDeck,
+        navigationOptions: {
+            tabBarLabel: 'New deck',
+            tabBarIcon: ({tintColor}) => <Ionicons name='ios-add' size={30} color={tintColor}/>
+        }
+    }
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        //activeTintColor: '#f26f28',
+        style: {
+            height: 56,
+            shadowColor: 'rgba(0,0,0,0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
+
+const MainNavigator = StackNavigator({
+    Home: {
+        screen: Tabs,
+    },
+    Deck: {
+        screen: Deck,
+        navigationOptions: {
+            //headerTintColor: white,
+            //headerStyle: {
+            //    backgroundColor: purple
+            //}
+        }
+    }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <View style={{height: Constants.statusBarHeight}}>
+              <StatusBar translucent barStyle='dark-content' backgroundColor='#434516'/>
+          </View>
+          <MainNavigator/>
       </View>
     );
   }
@@ -16,8 +74,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
