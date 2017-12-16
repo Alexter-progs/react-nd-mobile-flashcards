63c94825a11588b9d9f5c2ff94bb2f4a7b721042
alexp <alexplotbox@teamlead.ru>
2017-12-16 19:37:13 +0300

    fix class name

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-export default class DeckList extends Component {
+export default class NewDeck extends Component {
     render() {
         return(
             <View style={{flex: 1}}>
