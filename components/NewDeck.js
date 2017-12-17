4d9a30d7ffb95842dffef766d14efc6cbb3ed6c8
alexp <alexplotbox@teamlead.ru>
2017-12-17 22:09:30 +0300

    add new deck controls

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 
 export default class NewDeck extends Component {
+    state = {
+        deckTitle: ''
+    }
     render() {
         return(
-            <View style={{flex: 1}}>
-                <Text>New Deck</Text>
+            <View>
+                <Text>What is the title of your new deck</Text>
+                <TextInput style={{width: 200, height: 44, padding: 8, borderWidth: 1, margin: 50, borderColor: '#757575'}} value={this.state.text} onChangeText={(deckTitle) => this.setState({deckTitle})} placeholderTextColor='#4ac431'/>
+                <TouchableOpacity><Text>Submit</Text></TouchableOpacity>
             </View>
         )
     }
