143a145241c88b7a08fbdca1188c75537266954e
alexp <alexplotbox@teamlead.ru>
2017-12-19 19:25:50 +0300

    add ability to save decks

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 
+import { saveDeck } from '../storage/DAL'
+
 export default class NewDeck extends Component {
     state = {
         deckTitle: ''
     }
+
+    saveDeck = () => {
+        saveDeck(this.state.deckTitle)
+        this.props.navigation.navigate('Home')
+    }
+
     render() {
         return(
             <View>
                 <Text>What is the title of your new deck</Text>
                 <TextInput style={{width: 200, height: 44, padding: 8, borderWidth: 1, margin: 50, borderColor: '#757575'}} value={this.state.text} onChangeText={(deckTitle) => this.setState({deckTitle})} placeholderTextColor='#4ac431'/>
-                <TouchableOpacity><Text>Submit</Text></TouchableOpacity>
+                <TouchableOpacity onPress={this.saveDeck}>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
             </View>
         )
     }
