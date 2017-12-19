1daae53b2de6bad3e0f6c950d639fdf76a27cf7f
alexp <alexplotbox@teamlead.ru>
2017-12-19 19:19:30 +0300

    handle quiz when questions length is zero, pass full deck object to quiz

--- components/Deck.js
+++ components/Deck.js
@@ -3,17 +3,26 @@ import { View, Text, TouchableOpacity } from 'react-native'
 
 export default class DeckList extends Component {
     static navigationOptions = ({ navigation }) => ({
-        title: navigation.state.params.deckTitle
+        title: navigation.state.params.deck.title
     });
 
+    handleQuizStart = () => {
+        if(this.props.navigation.state.params.deck.questions.length <= 0) {
+            this.props.navigation.navigate('NewCard', { deckId: this.props.navigation.state.params.deckTitle})
+        } else {
+            this.props.navigation.navigate('Quiz', { questions: this.props.navigation.state.params.deck.questions})
+        }
+    }
+
     render() {
+
         return(
             <View>
                 <Text>Deck</Text>
                 <TouchableOpacity onPress={() => this.props.navigation.navigate('NewCard', { deckId: this.props.navigation.state.params.deckTitle})}>
                     <Text>Add new card</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('Quiz')}>
+                <TouchableOpacity onPress={this.handleQuizStart}>
                     <Text>Start Quiz</Text>
                 </TouchableOpacity>
             </View>
