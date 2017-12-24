c0b4db7f192678ba480eb8241d95f18c4c6b6962
alexp <alexplotbox@teamlead.ru>
2017-12-24 16:34:15 +0300

    add small animation for flipping question/answer

--- components/QuizItem.js
+++ components/QuizItem.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 
 import { red } from '../utils/colors'
 
 export default class QuizItem extends Component {
     state = {
-        isShowAnswer: false
+        isShowAnswer: false,
+        bounceValue: new Animated.Value(1)
     }
 
     showAnswer = () => {
+        this.showAnimation()
         this.setState(() => ({
             isShowAnswer: true
         }))
     }
 
     showQuestion = () => {
+        this.showAnimation()
         this.setState(() => ({
             isShowAnswer: false
         }))
+    }
+
+    showAnimation = () => {
+        const { bounceValue } = this.state
+        Animated.sequence([
+            Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start()
     }
 
     render() {
         if(!this.props.quizItem) {
             return (<View></View>)
         }
+
+        const { bounceValue, isShowAnswer } = this.state
+        const { answer, question } = this.props.quizItem
+
+        const quizText = isShowAnswer ? answer : question
         return (
             <View>
                 <View style={styles.itemContainer}>
-                {
-                    this.state.isShowAnswer ? (
-                        <Text style={styles.text}>{this.props.quizItem.answer}</Text>
-
-                    ) : (
-                        <Text style={styles.text}>{this.props.quizItem.question}</Text>
-
-                    )
-                }
+                    <Animated.Text style={[styles.text, { transform: [{scale: bounceValue}]}]}>{quizText}</Animated.Text>
                 </View>
+
                 <View>
                 { this.state.isShowAnswer ? (
                         <TouchableOpacity style={styles.flipCardBtn} onPress={this.showQuestion}>
